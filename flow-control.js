cf684bad4fe90412708636d5144192a55597f399
Leonardo Gadotti de Figueiredo <leonardo.of.g@gmail.com>
2019-09-04 20:00:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,6 +2,11 @@ function basicTeenager(age) {
 if (true) {
 true >=13 && true<=19
 return "You are a teenager!"
+}else{
+  if(false){
+    false<13 && false>19
+    return "You are not a teenager"
+  }
 }}
 
 function teenager(age) {
