483c9b930fb5821dd2ff51103c7b140a05eda829
Leonardo Gadotti de Figueiredo <leonardo.of.g@gmail.com>
2019-09-04 20:48:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-return basicTeenager? "You are a teenager" : "You are not a teenager"
+return teenager? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
